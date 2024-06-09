56aeead715ac8878b8c178205be8a30704eb19ae
Darius <darius.songaila1@gmail.com>
2024-06-09 22:44:11 +0300

    update

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tictactoe;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,78 +40,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main);
         winnerField = findViewById(R.id.winnerField);
         playAgain = findViewById(R.id.playAgain);
-        playAgain.setVisibility(View.INVISIBLE);
 
         createSquares();
         createSquareList();
-        for(TextView tv : squareList){
-            tv.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(player == 1){
-                        tv.setText("x");
-                        checkWinner();
-                        player = 2;
-                        tv.setOnClickListener(null);
-
-                    }else{
-                        tv.setText("o");
-                        checkWinner();
-                        player = 1;
-                        tv.setOnClickListener(null);
-
-                    }
-                }
-            });
-        }
-
+        setOnclickListeners();
     }
     public static void checkWinner(){
-        //Pridedu mygtukus prie matricos
-        String[][] matrix = new String[3][3];
-        int skaicius = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                matrix[i][j] = (String) squareList.get(skaicius).getText();
-                skaicius++;
-            }
-        }
-        for(int i = 0; i < 3; i++) {
-            if(Objects.equals(matrix[i][0], matrix[i][1]) && Objects.equals(matrix[i][2], matrix[i][0])
-                    && !Objects.equals(matrix[i][0], "")){
-                winnerField.setText("Winner is player " + player);
-                stopAction();
-                playAgain.setVisibility(View.VISIBLE);
-            } else if (Objects.equals(matrix[0][i], matrix[1][i])
-                    && Objects.equals(matrix[2][i], matrix[0][i]) && !Objects.equals(matrix[0][i], "")) {
-                winnerField.setText("Winner is player " + player);
-                stopAction();
-                playAgain.setVisibility(View.VISIBLE);
 
-            }
-
-        }
-        if(Objects.equals(matrix[0][0], matrix[1][1]) && Objects.equals(matrix[2][2], matrix[0][0]) &&
-                !Objects.equals(matrix[0][0], "")){
-            winnerField.setText("Winner is player " + player);
-            stopAction();
-            playAgain.setVisibility(View.VISIBLE);
-        } else if (Objects.equals(matrix[0][2], matrix[1][1]) && Objects.equals(matrix[2][0], matrix[0][2]) &&
-                !Objects.equals(matrix[0][2], "")) {
-            winnerField.setText("Winner is player " + player);
-            stopAction();
-            playAgain.setVisibility(View.VISIBLE);
-        }
-        int draw = 0;
-        for(TextView tv : squareList){
-            if(!tv.getText().equals("")){
-                draw++;
-            }
-        }
-        if(draw == 9){
-            winnerField.setText("Draw");
-            playAgain.setVisibility(View.VISIBLE);
-        }
+        String[][] ticTacToe = putSquaresToMatrix();
+        checkVerticalAndHorizontalLines(ticTacToe);
+        checkDiagonalLines(ticTacToe);
+        checkDraw();
     }
     public void createSquares(){
         firstSquare = findViewById(R.id.square1);
@@ -139,9 +79,108 @@ public class MainActivity extends AppCompatActivity {
             tv.setOnClickListener(null);
         }
     }
-    public void clearBlocks(){
+    public static void clearBlocks(){
         for(TextView tv : squareList){
             tv.setText("");
+        }
+    }
+    public static void playAgain(){
+        playAgain.setVisibility(View.VISIBLE);
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearBlocks();
+                winnerField.setText("");
+                player = 1;
+                setOnclickListeners();
+                playAgain.setVisibility(View.INVISIBLE);
+                playAgain.setOnClickListener(null);
+            }
+        });
+    }
+    public static void setOnclickListeners(){
+        for(TextView tv : squareList){
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(player == 1){
+                        tv.setTextColor(Color.RED);
+                        tv.setText("x");
+                        checkWinner();
+                        player = 2;
+                        tv.setOnClickListener(null);
+
+                    }else{
+                        tv.setTextColor(Color.BLUE);
+                        tv.setText("o");
+                        checkWinner();
+                        player = 1;
+                        tv.setOnClickListener(null);
+
+                    }
+                }
+            });
+        }
+    }
+    public static void declareWinner(){
+        winnerField.setText("Winner is player " + player);
+    }
+    public static String[][] putSquaresToMatrix(){
+        String[][] matrix = new String[3][3];
+        int skaicius = 0;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                matrix[i][j] = (String) squareList.get(skaicius).getText();
+                skaicius++;
+            }
+        }
+        return matrix;
+    }
+    public static void checkVerticalAndHorizontalLines(String[][] matrix){
+        for(int i = 0; i < 3; i++) {
+            if(Objects.equals(matrix[i][0], matrix[i][1])
+                    && Objects.equals(matrix[i][2], matrix[i][0])
+                    && !Objects.equals(matrix[i][0], ""))
+            {
+                declareWinner();
+                stopAction();
+                playAgain();
+            } else if (Objects.equals(matrix[0][i], matrix[1][i])
+                    && Objects.equals(matrix[2][i], matrix[0][i])
+                    && !Objects.equals(matrix[0][i], ""))
+            {
+                declareWinner();
+                stopAction();
+                playAgain();
+            }
+        }
+    }
+    public static void checkDiagonalLines(String[][] matrix){
+        if(Objects.equals(matrix[0][0], matrix[1][1])
+                && Objects.equals(matrix[2][2], matrix[0][0])
+                && !Objects.equals(matrix[0][0], "")){
+            declareWinner();
+            stopAction();
+            playAgain();
+        } else if (Objects.equals(matrix[0][2], matrix[1][1])
+                && Objects.equals(matrix[2][0], matrix[0][2])
+                && !Objects.equals(matrix[0][2], "")) {
+            declareWinner();
+            stopAction();
+            playAgain();
+        }
+    }
+    public static void checkDraw(){
+        int draw = 0;
+        for(TextView tv : squareList){
+            if(!tv.getText().equals("")){
+                draw++;
+            }
+        }
+        if(draw == 9){
+            winnerField.setText("Draw");
+            stopAction();
+            playAgain();
         }
     }
 }
